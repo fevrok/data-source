8e4a078770d0fe6ff953b7a8e318ea8f038e35c7
Abdellah Chadidi <chadidi97@gmail.com>
2017-03-02 19:30:28 +0000

    Combine States and Cities with Countries Migration

--- src/publishes/migrations/2017_02_27_140607_create_countries_table.php
+++ src/publishes/migrations/2017_02_27_140607_create_countries_table.php
@@ -18,6 +18,22 @@ class CreateCountriesTable extends Migration
             $table->string('code',2);
             $table->string('name');
         });
+        Schema::create('data_states', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('country_id')->unsigned();
+            $table->string('name');
+
+            $table->foreign('country_id')->references('id')->on('data_countries');
+        });
+        Schema::create('data_cities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('country_id')->unsigned();
+            $table->integer('state_id')->unsigned();
+            $table->string('name');
+
+            $table->foreign('country_id')->references('id')->on('data_countries');
+            $table->foreign('state_id')->references('id')->on('data_states');
+        });
     }
 
     /**
@@ -28,5 +44,7 @@ class CreateCountriesTable extends Migration
     public function down()
     {
         Schema::dropIfExists('data_countries');
+        Schema::dropIfExists('data_states');
+        Schema::dropIfExists('data_cities');
     }
 }
