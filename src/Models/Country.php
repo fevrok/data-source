63b105e21679b5c2e62158b61e1e08b550c2da5e
Abdellah Chadidi <chadidi97@gmail.com>
2017-03-05 16:52:15 +0000

    Update Countries Model

--- src/Models/Country.php
+++ src/Models/Country.php
@@ -1,11 +1,33 @@
 <?php
 
-namespace LaravelArab\DataSource;
+namespace LaravelArab\DataSource\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Config\Repository;
 
 class Country extends Model
 {
     protected $table = "data_countries";
+    public $currentLocale;
 
+    public function __construct()
+    {
+        $this->app = app();
+        $this->config = $this->app['config'];
+        $this->currentLocale = $this->config->get('app.locale');
+    }
+
+    public function states() {
+		return $this->hasMany('LaravelArab\DataSource\Models\State');
+	}
+	public function cities() {
+		return $this->hasMany('LaravelArab\DataSource\Models\City');
+	}
+	public function currentLocale() {
+
+	}
+	public function getNameAttribute($value) {
+	    $name = json_decode($value, true);
+	    return $name[$this->currentLocale];
+	}
 }
