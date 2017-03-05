5168d3e89bb76d201ded4f546f43fdda287ac134
Abdellah Chadidi <chadidi97@gmail.com>
2017-03-05 16:52:53 +0000

    Update Cities Model

--- src/Models/City.php
+++ src/Models/City.php
@@ -1,11 +1,18 @@
 <?php
 
-namespace LaravelArab\DataSource;
+namespace LaravelArab\DataSource\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class City extends Model
 {
     protected $table = "data_cities";
+
+    public function country(){
+		return $this->belongsTo('LaravelArab\DataSource\Models\Country');
+	}
+	public function state(){
+		return $this->belongsTo('LaravelArab\DataSource\Models\State');
+	}
 
 }
