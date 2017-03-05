9d5473b5e3f824d2bf4cdcbe5fc629bd0dfc35db
Abdellah Chadidi <chadidi97@gmail.com>
2017-03-05 16:52:35 +0000

    Update States Model

--- src/Models/State.php
+++ src/Models/State.php
@@ -1,11 +1,18 @@
 <?php
 
-namespace LaravelArab\DataSource;
+namespace LaravelArab\DataSource\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class State extends Model
 {
     protected $table = "data_states";
+
+    public function cities() {
+		return $this->hasMany('LaravelArab\DataSource\Models\City');
+	}
+	public function country(){
+		return $this->belongsTo('LaravelArab\DataSource\Models\Country');
+	}
 
 }
