02a8e969ebd311f89fe62e142d3d1cd82bb79ab1
Abdellah Chadidi <chadidi97@gmail.com>
2017-03-05 16:50:57 +0000

    Update Data And Add Methods

--- src/DataSource.php
+++ src/DataSource.php
@@ -3,14 +3,9 @@
 namespace LaravelArab\DataSource;
 
 use Illuminate\Config\Repository;
-// use Illuminate\Foundation\Application;
-// use Illuminate\Http\Request;
-// use Illuminate\Routing\Router;
-// use Illuminate\Support\Facades\URL;
-// use Illuminate\Translation\Translator;
-// use Illuminate\View\Factory;
-// use Mcamara\LaravelLocalization\Exceptions\SupportedLocalesNotDefined;
-// use Mcamara\LaravelLocalization\Exceptions\UnsupportedLocaleException;
+use LaravelArab\DataSource\Models\Country;
+use LaravelArab\DataSource\Models\State;
+use LaravelArab\DataSource\Models\City;
 
 class DataSource
 {
@@ -30,4 +25,53 @@ class DataSource
         $this->app = app();
         $this->config = $this->app['config'];
     }
+    // Countries
+    public function countries(){
+        return Country::All();
+    }
+    public function country($id){
+        return Country::find($id);
+    }
+    public function countryByCode($code){
+        return Country::where('code',$code)->first();
+    }
+    public function countryByName($name){
+        return Country::where('name','like',"%".$name."%")->first();
+    }
+    public function countriesByName($name){
+        return Country::where('name','like',"%".$name."%")->get();
+    }
+    // States
+    public function states(){
+        return State::All();
+    }
+    public function state($id){
+        return State::find($id);
+    }
+    public function stateByCode($code){
+        return State::where('code',$code)->first();
+    }
+    public function stateByName($name){
+        return State::where('name','like',"%".$name."%")->first();
+    }
+    public function statesByName($name){
+        return State::where('name','like',"%".$name."%")->get();
+    }
+    // Cities    
+    public function cities(){
+        return City::All();
+    }
+    public function city($id){
+        return City::find($id);
+    }
+    public function cityByCode($code){
+        return City::where('code',$code)->first();
+    }
+    public function cityByName($name){
+        return City::where('name','like',"%".$name."%")->first();
+    }
+    public function citiesByName($name){
+        return City::where('name','like',"%".$name."%")->get();
+    }
+
 }
