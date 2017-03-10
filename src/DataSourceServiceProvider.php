8ebfd0995f09dc41b5460f0e4a35a6d0c3a32093
Abdellah Chadidi <chadidi97@gmail.com>
2017-03-10 19:30:33 +0000

    Bind DataSource::class with Facades

--- src/DataSourceServiceProvider.php
+++ src/DataSourceServiceProvider.php
@@ -3,7 +3,6 @@
 namespace LaravelArab\DataSource;
 
 use Illuminate\Support\ServiceProvider;
-
 class DataSourceServiceProvider extends ServiceProvider
 {
     /**
@@ -14,9 +13,13 @@ class DataSourceServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/publishes/config/config.php' => config_path('datasource.php'),
+            __DIR__.'/publishes/config.php' => config_path('datasource.php'),
         ], 'config');
-        // include __DIR__.'/DataSource.php';
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                Commands\ImportDataCommand::class,
+            ]);
+        }
     }
 
     /**
@@ -26,17 +29,14 @@ class DataSourceServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $packageConfigFile = __DIR__.'/publishes/config/config.php';
+        $this->app->bind('datasource', function($app)
+        {
+            return new DataSource();
+        });
+        $packageConfigFile = __DIR__.'/publishes/config.php';
 
         $this->mergeConfigFrom(
             $packageConfigFile, 'datasource'
         );
-
-        /*$this->app->singleton(DataSource::class, function () {
-            return new DataSource();
-        });*/
-        // $this->app->make('LaravelArab\DataSource\DataSource.php');
-
-        $this->app->alias(DataSource::class, 'datasource');
     }
 }
